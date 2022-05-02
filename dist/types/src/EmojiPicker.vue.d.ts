f6f2efd60ae5e279cce435b8c8402b8d980d94de
ThomBos <thombos@MBP-di-ThomBos.station>
2022-05-02 19:17:57 +0200

    chore(emojipicker): builded

---
+++ dist/types/src/EmojiPicker.vue.d.ts
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+declare const _default: import("vue/types/vue").ExtendedVue<Vue, unknown, {
+    insert(emoji: string): void;
+}, {
+    emojis: Record<string, Record<string, string>>;
+}, {
+    search: string;
+    emojiTable: Record<string, Record<string, string>>;
+}>;
+export default _default;
