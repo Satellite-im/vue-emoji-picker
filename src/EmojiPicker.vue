a04db017c684e4c2f66a6a3859a8efc461f030e2
ThomBos <thombos@MBP-di-ThomBos.station>
2022-05-02 17:37:45 +0200

    modified emoji-picker library

--- src/EmojiPicker.vue
+++ src/EmojiPicker.vue
@@ -1,20 +1,10 @@
 <template>
   <div>
-    <slot
-      name="emoji-invoker"
-      :events="{ click: (e) => toggle(e) }"
-    ></slot>
-    <div
-      v-if="display.visible"
-      v-click-outside="hide"
-    >
       <slot
         name="emoji-picker"
         :emojis="emojis"
         :insert="insert"
-        :display="display"
       ></slot>
-    </div>
   </div>
 </template>
 
@@ -24,16 +14,6 @@
 
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#escaping
   const escapeRegExp = (s: string) => s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
-
-  type Data = {
-    display: {
-      x: number
-      y: number
-      visible: boolean
-    }
-  }
-
-  type ClickOutsideElement = HTMLElement & { __vueClickOutside__: ((e: MouseEvent) => void) | null }
 
   export default /*#__PURE__*/Vue.extend({
     name: 'EmojiPicker',
@@ -50,15 +30,6 @@
           return emojis
         },
       },
-    },
-    data(): Data {
-      return {
-        display: {
-          x: 0,
-          y: 0,
-          visible: false,
-        },
-      }
     },
     computed: {
       emojis(): Record<string, Record<string, string>> {
@@ -89,51 +60,6 @@
       insert(emoji: string): void {
         this.$emit('emoji', emoji)
       },
-      toggle(e: MouseEvent): void {
-        this.display.visible = ! this.display.visible
-        this.display.x = e.clientX
-        this.display.y = e.clientY
-      },
-      hide(): void {
-        this.display.visible = false
-      },
-      escape(e: KeyboardEvent): void {
-        if (this.display.visible === true && e.keyCode === 27) {
-          this.display.visible = false
-        }
-      },
-    },
-    directives: {
-      'click-outside': {
-        bind(el: ClickOutsideElement, binding: any) {
-          if (typeof binding.value !== 'function') {
-            return
-          }
-
-          const bubble = binding.modifiers.bubble
-          const handler = (e: any) => {
-            if (bubble || (! el.contains(e.target) && el !== e.target)) {
-              binding.value(e)
-            }
-          }
-          el.__vueClickOutside__ = handler
-
-          document.addEventListener('click', handler)
-        },
-        unbind(el: ClickOutsideElement) {
-          if (el.__vueClickOutside__ !== null) {
-            document.removeEventListener('click', el.__vueClickOutside__)
-
-            el.__vueClickOutside__ = null
-          }
-        },
-      } as any,
-    },
-    mounted() {
-      document.addEventListener('keyup', this.escape)
-    },
-    destroyed() {
-      document.removeEventListener('keyup', this.escape)
     },
   })
 </script>
